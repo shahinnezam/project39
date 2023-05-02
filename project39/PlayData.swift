58a557aca53c6fdc7458f32997e612d457a897b3
Shahin Nezam <shahinnezam@hotmail.com>
2023-05-02 12:11:16 -0700

    added plays.txt and successful tableview

--- project39/PlayData.swift
+++ project39/PlayData.swift
@@ -9,4 +9,12 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
+    
+    init() {
+        if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
+            if let plays = try? String(contentsOfFile: path) {
+                allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
+            }
+        }
+    }
 }
