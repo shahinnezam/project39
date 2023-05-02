8b856129fa405f50a086f8ae5bcf8fbfe1816a46
Shahin Nezam <shahinnezam@hotmail.com>
2023-05-02 12:02:25 -0700

    first all words test passed

---
+++ project39/PlayData.swift
@@ -0,0 +1,12 @@
+//
+//  PlayData.swift
+//  project39
+//
+//  Created by Student Account on 5/2/23.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
--- project39Tests/project39Tests.swift
+++ project39Tests/project39Tests.swift
@@ -10,27 +10,17 @@ import XCTest
 
 class project39Tests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAllWordsLoaded(){
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
